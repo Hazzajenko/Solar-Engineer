1679b40da0e2395dbb9c0f02da2361b854f9a474
Harry Jenkins <jenkinsh1@outlook.com>
2022-12-31 14:22:10 +1100

    moved grid layout component to new lib

--- nxworkspace/libs/grid-layout/feature/index/jest.config.ts
+++ nxworkspace/libs/grid-layout/feature/index/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'grid-layout-feature',
-  preset: '../../../jest.preset.js',
+  displayName: 'grid-layout-feature-index',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/grid-layout/feature',
+  coverageDirectory: '../../../../coverage/libs/grid-layout/feature/index',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
