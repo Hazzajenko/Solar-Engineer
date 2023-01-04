444e2e6a3c079ec16f2e87f3de16b21ec4059969
Harry Jenkins <jenkinsh1@outlook.com>
2023-01-04 21:39:01 +1100

    added in scroll wheel zoom

--- nxworkspace/libs/grid-layout/feature/index/src/lib/directives/grid.directive.ts
+++ nxworkspace/libs/grid-layout/feature/index/src/lib/directives/grid.directive.ts
@@ -4,10 +4,13 @@ import {
   ElementRef,
   EventEmitter,
   HostListener,
-  inject,
-  Output,
+  inject, Input,
+  Output, Renderer2,
 } from '@angular/core'
-import { ElementOffsets } from '@grid-layout/shared/models'
+import { ElementOffsets, GridLayoutXY } from '@grid-layout/shared/models'
+import { UiRepository } from '@project-id/data-access/repositories'
+import { PanelPathModel, SoftColor } from '@shared/data-access/models'
+import { firstValueFrom } from 'rxjs'
 
 @Directive({
   selector: '[appGrid]',
@@ -15,8 +18,86 @@ import { ElementOffsets } from '@grid-layout/shared/models'
 })
 export class GridDirective implements AfterViewInit {
   private elementRef = inject(ElementRef<HTMLDivElement>)
+  private renderer = inject(Renderer2)
+  private uiRepository = inject(UiRepository)
+  private _zoomLevel = 1
+  private _componentX?: number
+  private _componentY?: number
+  // private speed = 0.5
+  public getScreenWidth: any
+  public getScreenHeight: any
+  private mouseClick!: { x: number, y: number, left: number, top: number }
+  private posX = 0
+  private posY = 0
+  private scale = 1
+  private speed = 0.1
+  private max_scale = 4
+  private min_scale = 1
+  top = 0
+  left = 0
+  isMoving = false // Flag to indicate
+  // whether the component is being moved
+  mouseX = 0 // X coordinate of the mouse
+  mouseY = 0 // Y coordinate of the mouse
+  componentX = 0 // X coordinate of the component
+  componentY = 0 // Y coordinate of the component
 
   @Output() elementOffsets: EventEmitter<ElementOffsets> = new EventEmitter<ElementOffsets>()
+
+  @Input() set gridlayoutComponentXy(gridLayoutXY: GridLayoutXY | undefined | null) {
+    if (!gridLayoutXY || !gridLayoutXY.componentX || !gridLayoutXY.componentY) return
+    console.log(gridLayoutXY)
+    this._componentX = gridLayoutXY.componentX
+    this._componentY = gridLayoutXY.componentY
+    console.log(gridLayoutXY)
+    /*    this.renderer.setStyle(
+          this.elementRef.nativeElement,
+          'transform',
+          `scale(${this._zoomLevel}) translate(${gridLayoutXY.componentX}px, ${gridLayoutXY.componentY}px)`,
+          // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+        )*/
+    const sizeH = this.elementRef.nativeElement.offsetHeight
+    const sizeW = this.elementRef.nativeElement.offsetWidth
+    if (this.posX > 0) this.posX = 0
+    if (this.posX + sizeW * this.scale < sizeW) this.posX = -sizeW * (this.scale - 1)
+    if (this.posY > 0) this.posY = 0
+    if (this.posY + sizeH * this.scale < sizeH) this.posY = -sizeH * (this.scale - 1)
+    /*    let compX = gridLayoutXY.componentX
+        let compY = gridLayoutXY.componentY
+        if (compX > 0) compX = 0
+        if (compX + sizeW * this.scale < sizeW) this.posX = -sizeW * (this.scale - 1)
+        if (compY > 0) compY = 0
+        if (compY + sizeH * this.scale < sizeH) this.posY = -sizeH * (this.scale - 1)*/
+    // const renderX = (gridLayoutXY.componentX < 0) ? gridLayoutXY.componentX + this.posX : gridLayoutXY.componentX - this.posX
+    // const renderY = (gridLayoutXY.componentY < 0) ? gridLayoutXY.componentY + this.posY : gridLayoutXY.componentY - this.posY
+    // const renderX = gridLayoutXY.componentX + this.posX
+    // const renderY = gridLayoutXY.componentY + this.posY
+    const renderX = gridLayoutXY.componentX
+    const renderY = gridLayoutXY.componentY
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'transform',
+      `translate(${renderX}px,${renderY}px) scale(${this.scale})`,
+      // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+    )
+    // console.log('gridLayoutXY.componentX + this.posX', gridLayoutXY.componentX + this.posX)
+    // console.log('gridLayoutXY.componentY + this.posY', gridLayoutXY.componentY + this.posY)
+  }
+
+
+  @HostListener('window:keyup', ['$event'])
+  async keyEvent(event: KeyboardEvent) {
+    console.log(event)
+    if (event.key === 'b') {
+      this.renderer.setStyle(
+        this.elementRef.nativeElement,
+        'transform',
+        `translate(${this.posX + 10}px,${this.posY}px) scale(${this.scale})`,
+        // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+      )
+    }
+
+  }
 
   ngAfterViewInit() {
     const offsets: ElementOffsets = {
@@ -38,8 +119,241 @@ export class GridDirective implements AfterViewInit {
       offsetTop: this.elementRef.nativeElement.offsetTop,
     }
     this.elementOffsets.emit(offsets)
-    // this.canvas.nativeElement.width = window.innerWidth
-    // this.canvas.nativeElement.height = window.innerHeight
-    // event.target.innerWidth;
   }
+
+  @HostListener('wheel', ['$event'])
+  public onScroll(event: WheelEvent) {
+    console.log(event)
+    event.preventDefault()
+    // this.elementRef.nativeElement.style.zoom = 2
+    this._zoomLevel += event.deltaY * 0.001
+    this._zoomLevel = Math.max(0.5, Math.min(this._zoomLevel, 2))
+    this.mouseX = event.clientX
+    this.mouseY = event.clientY
+    this.componentX = this.mouseX - this.elementRef.nativeElement.offsetWidth / 2 * this._zoomLevel
+    this.componentY = this.mouseY - this.elementRef.nativeElement.offsetHeight / 2 * this._zoomLevel
+    // this.updateZoom()
+    /*    this.renderer.setStyle(
+          this.elementRef.nativeElement,
+          'transform',
+          `scale(${this._zoomLevel}) translate(${this.componentX}px, ${this.componentY}px)`,
+          // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+        )*/
+    // this.element.nativeElement.scrollLeft += event.deltaY;
+
+
+    const sizeH = this.elementRef.nativeElement.offsetHeight
+    const sizeW = this.elementRef.nativeElement.offsetWidth
+
+    const pointerX = event.pageX - this.elementRef.nativeElement.offsetLeft
+    const pointerY = event.pageY - this.elementRef.nativeElement.offsetTop
+    const targetX = (pointerX - this.posX) / this.scale
+    const targetY = (pointerY - this.posY) / this.scale
+
+    this.scale += -1 * Math.max(-1, Math.min(1, event.deltaY)) * this.speed * this.scale
+
+
+    this.scale = Math.max(this.min_scale, Math.min(this.max_scale, this.scale))
+
+    this.posX = -targetX * this.scale + pointerX
+    this.posY = -targetY * this.scale + pointerY
+
+    if (this.posX > 0) this.posX = 0
+    if (this.posX + sizeW * this.scale < sizeW) this.posX = -sizeW * (this.scale - 1)
+    if (this.posY > 0) this.posY = 0
+    if (this.posY + sizeH * this.scale < sizeH) this.posY = -sizeH * (this.scale - 1)
+
+    /*    const xs = (event.clientX - this.posX) / this.scale
+        const ys = (event.clientY - this.posY) / this.scale
+        const delta = ((event as any).wheelDelta ? (event as any).wheelDelta : -event.deltaY);
+        (delta > 0) ? (this.scale *= 1.1) : (this.scale /= 1.1)
+        this.posX = event.clientX - xs * this.scale
+        this.posY = event.clientY - ys * this.scale*/
+    this.uiRepository.setPosXY({
+      posX: this.posX,
+      posY: this.posY,
+    })
+
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'transform',
+      `translate(${this.posX}px,${this.posY}px) scale(${this.scale})`,
+      // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+    )
+    console.log('this.posX', this.posX)
+    console.log('this.posY', this.posY)
+    // image.style.transform = `translate(${pos.x}px,${pos.y}px) scale(${scale},${scale})`
+
+  }
+
+  private updateZoom() {
+    console.log(this._componentX, this._componentY)
+    if (!this._componentX || this._componentY) {
+      this.renderer.setStyle(
+        this.elementRef.nativeElement,
+        'transform',
+        `scale(${this._zoomLevel})`,
+      )
+    } else {
+      this.renderer.setStyle(
+        this.elementRef.nativeElement,
+        'transform',
+        `scale(${this._zoomLevel}) translate(${this._componentX}px, ${this._componentY}px)`,
+        // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+      )
+    }
+
+  }
+
+  /*
+    @HostListener('document:mousedown', ['$event'])
+    mouseDown(event: MouseEvent) {
+      this.getScreenWidth = window.innerWidth
+      this.getScreenHeight = window.innerHeight
+      console.log(`${this.getScreenWidth}x${this.getScreenHeight}`)
+      console.log(event)
+      console.log('MOUSEDOWN', event)
+      if (event.button === 1 || event.ctrlKey) { // Check if the mouse wheel button was pressed
+        this.isMoving = true
+        this.mouseClick = {
+          x: event.clientX,
+          y: event.clientY,
+          left: this.left,
+          top: this.top,
+        }
+        this.left = this.mouseClick.left +
+          (this.mouseX - this.mouseClick.x)
+        this.top = this.mouseClick.top +
+          (this.mouseY - this.mouseClick.y)
+        /!*
+
+
+              this.startX = clientXY.clientX - rect.left
+              this.startY = clientXY.clientY - rect.top*!/
+        // const rect = this.elementRef.nativeElement.getBoundingClientRect()
+        /!*      this.mouseX = event.clientX
+              this.mouseY = event.clientY*!/
+        const rect = this.elementRef.nativeElement.getBoundingClientRect()
+        console.log(rect)
+        /!*      rel: {
+                x: e.pageX - pos.left,
+                  y: e.pageY - pos.top
+              }*!/
+
+        // this.mouseX = event.pageX - rect.left
+        // this.mouseY = event.pageY - rect.top
+        // this.mouseX = event.pageX - this.elementRef.nativeElement.parentNode.offsetLeft
+        // this.mouseY = event.pageY - this.elementRef.nativeElement.parentNode.offsetTop
+        // this.mouseX = event.clientX - rect.left
+        // this.mouseY = event.clientY - rect.top
+        this.isMoving = true
+        this.mouseX = event.clientX
+        this.mouseY = event.clientY
+        // this.componentX = event.target.offsetLeft;
+        // this.componentY = event.target.offsetTop;
+        // this.mouseX = event.clientX
+        // this.mouseY = event.clientY
+        const element = event.target as HTMLElement
+        console.log(element)
+        this.componentX = element.offsetLeft
+        this.componentY = element.offsetTop
+        console.log(this.mouseX, this.mouseY)
+        // this.componentX = (event.target as any).offsetLeft
+        // this.componentY = (event.target as any).offsetTop
+        console.log((event.target as any).offsetLeft, (event.target as any).offsetTop)
+        console.log(event)
+        /!*      this.componentX = event.target?.offsetLeft
+              this.componentY = event.target?.offsetTop*!/
+      }
+      /!*    console.log(event.clientX, event.clientY)
+          this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height)
+          this.startX = undefined
+          this.startY = undefined*!/
+      // return
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  */
+
+  /*
+    @HostListener('document:mouseup', ['$event'])
+    mouseUp(event: MouseEvent) {
+      event.preventDefault()
+      event.stopPropagation()
+      this.isMoving = false
+      return
+    }
+  */
+
+  /*<div [ngStyle]="{'transform': 'scale(' + zoomLevel + ') translate(' + componentX + 'px, ' + componentY + 'px)'}">
+      <!-- Your component content goes here -->
+  </div>*/
+  /*  @HostListener('document:mousemove', ['$event'])
+    onDragging(event: MouseEvent) {
+
+      // console.log(event)
+      if (event.ctrlKey && this.isMoving) {
+        const { left, top } = this.elementRef.nativeElement.getBoundingClientRect()
+        // const mouseX = event.pageX - this.elementRef.nativeElement.parentNode.offsetLeft
+        // const mouseY = event.pageY - this.elementRef.nativeElement.parentNode.offsetTop
+
+        const dx = event.clientX - this.mouseX
+        const dy = event.clientY - this.mouseY
+        // console.log(event)
+        // Calculate the new position of the component
+        /!*            const dx = event.clientX - this.mouseX
+                    const dy = event.clientY - this.mouseY
+                    this.componentX += dx
+                    this.componentY += dy*!/
+        // const dx = event.clientX - this.mouseX
+        // const dy = event.clientY - this.mouseY
+        const scaledDx = dx * this.speed
+        const scaledDy = dy * this.speed
+        /!*      let componentX = [...gridLayoutXY.componentX];
+              let componentY = [...y];*!/
+        // const updatedComponentX = gridLayoutXY.componentX + scaledDx
+        // const updatedComponentY = gridLayoutXY.componentY + scaledDy
+        this.componentX += scaledDx
+        this.componentY += scaledDy
+        // const mouseX = event.pageX - this.elementRef.nativeElement.parentNode.offsetLeft
+        // const mouseY = event.pageY - this.elementRef.nativeElement.parentNode.offsetTop
+        /!*      const dx = event.clientX - mouseX
+              const dy = event.clientY - mouseY
+              this.componentX += dx
+              this.componentY += dy*!/
+
+        // {'left': componentX + 'px', 'top': componentY + 'px'}
+        // this.elementRef.nativeElement.style.offsetLeft = this.componentX
+        // this.elementRef.nativeElement.style.offsetTop = this.componentY
+        console.log(this.elementRef.nativeElement.style.offsetLeft, this.elementRef.nativeElement.style.offsetTop)
+        /!*      this.renderer.setStyle(
+                this.elementRef.nativeElement,
+                'transform',
+                `scale(${this._zoomLevel}) translate(${this.componentX}px, ${this.componentY}px)`,
+              )*!/
+        // this.left = this.mouseClick.left + (this.mouse.x - this.mouseClick.x)
+        /!*      this.left = this.mouseClick.left +
+                (this.mouseX - this.mouseClick.x)
+              this.top = this.mouseClick.top +
+                (this.mouseY - this.mouseClick.y)*!/
+        this.left = this.mouseClick.left +
+          (event.clientX - this.mouseClick.x)
+        this.top = this.mouseClick.top +
+          (event.clientY - this.mouseClick.y)
+        // this.top = this.mouseClick.top + (this.mouse.y - this.mouseClick.y)
+        this.renderer.setStyle(
+          this.elementRef.nativeElement,
+          'transform',
+          `scale(${this._zoomLevel}) translate(${this.left}px, ${this.top}px)`,
+          // `scale(${this._zoomLevel}) translate3d(${this.left}px, ${this.top}px, 0px)`,
+        )
+
+        this.mouseX = event.clientX
+        this.mouseY = event.clientY
+
+        event.preventDefault()
+        event.stopPropagation()
+      }
+    }*/
+
 }
--- nxworkspace/libs/grid-layout/feature/index/src/lib/grid-layout.component.ts
+++ nxworkspace/libs/grid-layout/feature/index/src/lib/grid-layout.component.ts
@@ -1,15 +1,17 @@
-import { DragDropModule } from '@angular/cdk/drag-drop'
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop'
 import { CommonModule } from '@angular/common'
-import { Component, inject, Input } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, HostListener, inject, Input, ViewChild } from '@angular/core'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { ClickService, DropService, MouseService } from '@grid-layout/data-access/services'
+import { MouseEventRequest } from '@grid-layout/data-access/utils'
 import { KeymapOverlayComponent } from '@grid-layout/feature/keymap'
 import { StringTotalsOverlayComponent } from '@grid-layout/feature/string-stats'
-import { ControllerEvent, ElementOffsets } from '@grid-layout/shared/models'
+import { ControllerEvent, ElementOffsets, GridLayoutXY, MouseXY } from '@grid-layout/shared/models'
 import { LetModule } from '@ngrx/component'
 import { GridFacade, SelectedFacade, StringsFacade } from '@project-id/data-access/facades'
 import { BlockModel, StringModel } from '@shared/data-access/models'
 import { DoubleClickService } from 'libs/grid-layout/data-access/services/src/lib/double-click.service'
+import { MoveService } from 'libs/grid-layout/data-access/services/src/lib/move.service'
 import { UiFacade } from 'libs/project-id/data-access/facades/src/lib/ui.facade'
 import { Observable, switchMap } from 'rxjs'
 import { map } from 'rxjs/operators'
@@ -19,6 +21,13 @@ import { GridDirective } from './directives/grid.directive'
 import { KeyMapDirective } from './directives/key-map.directive'
 import { GetBlockPipe } from './pipes/get-block.pipe'
 import { GetLocationPipe } from './pipes/get-location.pipe'
+
+
+const enum Status {
+  OFF = 0,
+  RESIZE = 1,
+  MOVE = 2
+}
 
 @Component({
   selector: 'app-grid-layout',
@@ -37,19 +46,99 @@ import { GetLocationPipe } from './pipes/get-location.pipe'
   ],
   templateUrl: './grid-layout.component.html',
   hostDirectives: [KeyMapDirective],
-  styles: [],
+  styles: [`
+    * {
+      padding: 0;
+      margin: 0;
+      outline: 0;
+      overflow: hidden;
+    }
+
+    html, body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+    }
+
+    /*
+
+    #zoom {
+      width: 100%;
+      height: 100%;
+      transform-origin: 0px 0px;
+      transform: scale(1) translate(0px, 0px);
+      !*cursor: grab;*!
+    }
+
+    div#zoom > #appGrid {
+      width: 100%;
+      height: auto;
+    }
+    */
+
+
+    .appGrid {
+      width: 100%;
+      height: 100%;
+      transition: transform .1s;
+      transform-origin: 0 0;
+    }
+
+    #appGrid {
+      width: auto;
+      height: auto;
+      max-width: 100%;
+    }
+  `],
 })
 export class GridLayoutComponent {
   public clickService = inject(ClickService)
   public dropService = inject(DropService)
   public mouseService = inject(MouseService)
+  public moveService = inject(MoveService)
   public doubleClickService = inject(DoubleClickService)
   private gridFacade = inject(GridFacade)
   private uiFacade = inject(UiFacade)
   private snackBar = inject(MatSnackBar)
   private selectedFacade = inject(SelectedFacade)
   private stringsFacade = inject(StringsFacade)
-  showKeymap$ = this.uiFacade.keymap$
+  public getScreenWidth: any
+  public getScreenHeight: any
+  // private uiFacade = inject(UiFacade)
+  showKeymap$ = this.uiFacade.isKeyMapEnabled$
+  zoomLevel = 2
+  isGridMoving = false
+
+  constructor() {
+    this.getScreenWidth = window.innerWidth
+    this.getScreenHeight = window.innerHeight
+    console.log(`${this.getScreenWidth}x${this.getScreenHeight}`)
+  }
+
+
+  @Input() rows!: number
+  @Input() cols!: number
+  @Input() blocks$!: Observable<BlockModel[]>
+
+  gridMode$ = this.gridFacade.gridMode$
+  clientXY$ = this.gridFacade.clientXY$
+  gridLayoutXY$ = this.uiFacade.gridLayoutXY$
+  gridLayoutXY: GridLayoutXY = {
+    componentX: undefined,
+    componentY: undefined,
+  }
+
+  mouseXY: MouseXY = {
+    mouseX: undefined,
+    mouseY: undefined,
+  }
+
+  offsets: ElementOffsets = {
+    offsetHeight: undefined,
+    offsetWidth: undefined,
+  }
 
   selectedString$: Observable<StringModel | undefined> = this.selectedFacade.selectedStringId$.pipe(
     switchMap(stringId => this.stringsFacade.stringById$(stringId).pipe(
@@ -60,16 +149,12 @@ export class GridLayoutComponent {
     )),
   )
 
-  @Input() rows!: number
-  @Input() cols!: number
-  @Input() blocks$!: Observable<BlockModel[]>
-
-  gridMode$ = this.gridFacade.gridMode$
-  clientXY$ = this.gridFacade.clientXY$
-
-  offsets: ElementOffsets = {
-    offsetHeight: undefined,
-    offsetWidth: undefined,
+  listDragOver(e: any, blocks: BlockModel[]) {
+    if (e.dragItem && Array.isArray(e.dragItem)) {
+      const targetIndex = blocks.indexOf(e.targetItem)
+      if (targetIndex <= 2)
+        e.cancel = true
+    }
   }
 
   numSequence(n: number): Array<number> {
@@ -81,4 +166,77 @@ export class GridLayoutComponent {
       duration,
     })
   }
+
+  async doubleCLick(doubleClick: MouseEventRequest) {
+    await this.doubleClickService.doubleCLick(doubleClick)
+  }
+
+  async mouse(mouse: MouseEventRequest) {
+    if (mouse.event.type === 'mouseup') {
+      this.isGridMoving = false
+    }
+    console.log(mouse.event)
+
+    if (mouse.event.button === 1 || mouse.event.ctrlKey) {
+      if (mouse.event.type === 'mousedown') {
+        this.isGridMoving = true
+        const posXY = await this.uiFacade.posXY
+        // start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        if (posXY && posXY.posX && posXY.posY) {
+
+          const mouseX = mouse.event.clientX - posXY.posX
+          const mouseY = mouse.event.clientY - posXY.posY
+          // start = { x: e.clientX - pointX, y: e.clientY - pointY }
+          this.mouseXY = {
+            mouseX,
+            mouseY,
+          }
+        } else {
+          const mouseX = mouse.event.clientX
+          const mouseY = mouse.event.clientY
+          // start = { x: e.clientX - pointX, y: e.clientY - pointY }
+          this.mouseXY = {
+            mouseX,
+            mouseY,
+          }
+        }
+        /*        const mouseX = mouse.event.clientX
+                const mouseY = mouse.event.clientY*/
+
+        return
+      }
+      if (mouse.event.type === 'mouseup') {
+        this.isGridMoving = false
+      }
+    }
+    await this.mouseService.mouse(mouse)
+  }
+
+  async click(click: MouseEventRequest) {
+    await this.clickService.click(click)
+  }
+
+  async drop(drop: CdkDragDrop<BlockModel[]>) {
+    await this.dropService.drop(drop)
+  }
+
+  async move(move: MouseEventRequest) {
+    move.event.preventDefault()
+    if (this.isGridMoving && move.event.ctrlKey) {
+      console.log(move.event)
+      if (!this.mouseXY || !this.mouseXY.mouseX || !this.mouseXY.mouseY) return undefined
+      const componentX = move.event.clientX - this.mouseXY.mouseX
+      const componentY = move.event.clientY - this.mouseXY.mouseY
+      this.gridLayoutXY = {
+        componentX,
+        componentY,
+      }
+      return
+    }
+    if (this.isGridMoving && !move.event.ctrlKey) {
+      this.isGridMoving = false
+    }
+    return
+  }
+
 }
