e99fabd2f22ec995b21fe0256d507abe9cbe1b1c
Harry Jenkins <jenkinsh1@outlook.com>
2023-03-25 16:13:13 +1100

    changed start up scripts

--- run-identity-api.ps1
+++ run-identity-api.ps1
@@ -1,7 +1,7 @@
 ﻿
 $location = [string](Get-Location)
 # // Write-Host "dotnet run --project ${location}\src\Services\Auth.API\Auth.API.csproj"
-Write-Host "dotnet run --project ${location}\src\Services\Identity.API\Identity.API.csproj"
+Write-Host "dotnet run --project ${location}\src\Services\Identity\Identity.API\Identity.API.csproj"
 
-dotnet run --project ${location}\src\Services\Identity.API\Identity.API.csproj --launch-profile Identity-Https-V2
+dotnet run --project ${location}\src\Services\Identity\Identity.API\Identity.API.csproj --launch-profile Identity-Https-V2
 # // dotnet run --project ${location}\src\Services\Auth.API\Auth.API.csproj --launch-profile Auth-Https-V2
--- run-projects-api.ps1
+++ run-projects-api.ps1
@@ -1,6 +1,6 @@
 ﻿
 $location = [string](Get-Location)
-$projectLocation = "${location}\src\Services\Projects.API\Projects.API.csproj"
+$projectLocation = "${location}\src\Services\Projects\Projects.API\Projects.API.csproj"
 Write-Host "dotnet run --project $projectLocation --launch-profile Projects-Https-V2"
 # dotnet build --project $projectLocation
 dotnet run --project $projectLocation --launch-profile Projects-Https-V2
