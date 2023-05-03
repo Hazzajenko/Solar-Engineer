2e3f6b4a365a490055b1d6c37db86daf4a9c5a09
Harry Jenkins <jenkinsh1@outlook.com>
2023-05-04 08:17:26 +1000

    migrate to nx 16

--- src/WebUI/apps/web/tailwind.config.js
+++ src/WebUI/apps/web/tailwind.config.js
@@ -1,18 +1,18 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind')
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind')
 const { join } = require('path')
 const sharedTailwindConfig = require('../../libs/tailwind-preset/tailwind.config')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [sharedTailwindConfig],
-  content: [
-    join(__dirname, 'src/**/*.{ts,html}'),
-    join('libs/design-app/feature-design-layout/src/lib/**/*.{ts,html}'),
-    join('libs/design-app/feature-panel/src/lib/**/*.{ts,html}'),
-    join('libs/grid-layout/feature/src/lib/**/*.{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  /*  darkMode: 'class',
+	presets: [sharedTailwindConfig],
+	content: [
+		join(__dirname, 'src/**/*.{ts,html}'),
+		join('libs/design-app/feature-design-layout/src/lib/**/*.{ts,html}'),
+		join('libs/design-app/feature-panel/src/lib/**/*.{ts,html}'),
+		join('libs/grid-layout/feature/src/lib/**/*.{ts,html}'),
+		...createGlobPatternsForDependencies(__dirname),
+	],
+	/*  darkMode: 'class',
     theme: {
       extend: {
         colors: {
