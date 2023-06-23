c3edeea5cf30668e3d5c0eebb090fd8487ad7e06
Harry Jenkins <jenkinsh1@outlook.com>
2023-06-23 16:15:39 +1000

    added in seq for logging

---
+++ run-seq.ps1
@@ -0,0 +1,10 @@
+﻿docker run `
+    --name seq `
+    -d `
+    --restart unless-stopped `
+    -e ACCEPT_EULA=Y `
+    -e SEQ_FIRSTRUN_ADMINPASSWORDHASH="$PH" `
+    -v ./seq-data:/data `
+    -p 80:80 `
+    -p 5341:5341 `
+    datalust/seq
