02ca14fdd993578bc3605e3de79cf454686ac64a
Harry Jenkins <jenkinsh1@outlook.com>
2023-07-03 14:25:02 +1000

    organised repo root folder, created api-gateways folder for envoy, nginx and traefik. created docker folder for compose files. only scripts folder to specific topics

--- scripts/deployment/move-cert-to-vm.ps1
+++ scripts/deployment/move-cert-to-vm.ps1
@@ -3,24 +3,6 @@ $certFile = "solarengineer.vault.pfx"
 $certWindowsDir = "${location}/https/${certFile}"
 $certLinuxDir = "/home/docker/https/${certFile}"
 
-<# $linuxSsh
-$privateKey
-Get-Content .env | ForEach-Object {
-    $name, $value = $_.split('=')
-    if ([string]::IsNullOrWhiteSpace($name) || $name.Contains('#')) {
-        # continue
-    }
-    else {
-        if ($name -eq "VM_SSH") {
-            $linuxSsh = $value
-        }
-        if ($name -eq "PRIVATE_KEY") {
-            $privateKey = $value
-        }
-    }
-
-} #>
-
 $envContent = Get-Content .env
 
 $linuxSsh = ($envContent | Select-String -Pattern "^VM_SSH=(.*)$").Matches.Groups[1].Value
