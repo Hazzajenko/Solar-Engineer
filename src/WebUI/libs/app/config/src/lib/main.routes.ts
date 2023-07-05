5bdd9bf5572afb5fd6b760569d951e912836ad07
Harry Jenkins <jenkinsh1@outlook.com>
2023-07-05 18:33:10 +1000

    add title to app, and remove old sign in routes

--- src/WebUI/libs/app/config/src/lib/main.routes.ts
+++ src/WebUI/libs/app/config/src/lib/main.routes.ts
@@ -3,22 +3,22 @@ import { Route } from '@angular/router'
 export const appRoutes: Route[] = [
 	{
 		path: '',
+		title: 'Solar Engineer',
 		loadComponent: () => import('@canvas/app/feature').then((m) => m.DesignCanvasAppComponent),
-	},
-	{
-		path: 'sign-in',
-		loadComponent: () => import('@auth/feature').then((m) => m.SignInComponent),
-	},
-	{
-		path: 'sign-in-v2',
-		loadComponent: () => import('@auth/feature').then((m) => m.SignInV2Component),
-	},
-	{
-		path: 'sign-in2',
-		loadComponent: () => import('@auth/feature').then((m) => m.SignInCenterComponent),
-	},
-	{
-		path: 'sign-in3',
-		loadComponent: () => import('@auth/feature').then((m) => m.SignInDialog),
-	},
+	}, // {
+	// 	path: 'sign-in',
+	// 	loadComponent: () => import('@auth/feature').then((m) => m.SignInComponent),
+	// },
+	// {
+	// 	path: 'sign-in-v2',
+	// 	loadComponent: () => import('@auth/feature').then((m) => m.SignInV2Component),
+	// },
+	// {
+	// 	path: 'sign-in2',
+	// 	loadComponent: () => import('@auth/feature').then((m) => m.SignInCenterComponent),
+	// },
+	// {
+	// 	path: 'sign-in3',
+	// 	loadComponent: () => import('@auth/feature').then((m) => m.SignInDialog),
+	// },
 ]
